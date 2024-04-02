29e13989c15a39d1891864076bd07e97dcb24266
nikhilbishnoi-473 <nikhilbishnoibhadu@gmail.com>
2024-04-02 11:56:28 -1200

    slick slider done

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,3 +37,38 @@ $('.responsive').slick({
     
     ]
   });
+  
+$('.responsive_2').slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  autoplay: false,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+         
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  
+  ]
+});
