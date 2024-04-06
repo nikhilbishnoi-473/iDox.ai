a7b37a43bfb6b1508869100dcf1ccb499a7ff244
nikhilbishnoi-473 <nikhilbishnoibhadu@gmail.com>
2024-04-05 16:27:47 -1200

    page done

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,29 +36,7 @@ $('.responsive').slick({
   pauseOnHover: true,
   cssEase: 'linear',
     responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 6,
-          slidesToScroll: 1,
-          infinite: true,
-           
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 5,
-          slidesToScroll: 1
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1
-        }
-      }
+     
     
     ]
   });
